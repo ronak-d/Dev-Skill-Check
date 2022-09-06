e7fd02cc766c5accada27efe78eee05b9dd008fd
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-09-07 01:40:47 +0530

    ques1 done

--- Js-1.js
+++ Js-1.js
@@ -1,3 +1,27 @@
 // 1. Write a program to find whether a given year is a leap year or not.
 
+function isLeapYear(year){
+    if(year%4 == 0) {
+        console.log(`${year} is a leap year`)
+    }
+    else{
+        console.log(`this year ${year} is not a leap year`)
+    }
+}
 
+isLeapYear(2022);
+
+
+
+// 2. Write a JavaScript program to convert temperatures to and from Celsius,
+// Fahrenheit.
+// [ Formula : c/5 = (f-32)/9 [ where c = temperature in Celsius and f = temperature in
+// Fahrenheit ]
+// Expected Output :
+// 60°C is 140 °F
+// 45°F is 7.222222222222222°C
+
+
+
+
+// 3. Write a program to find the factorial of a number.

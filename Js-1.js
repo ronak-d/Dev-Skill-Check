d29af331d7af9739ea1aaa65ef4601e7ee6e3cbe
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-09-07 14:14:15 +0530

    2nd ques done

--- Js-1.js
+++ Js-1.js
@@ -9,17 +9,28 @@ function isLeapYear(year){
     }
 }
 
-isLeapYear(2022);
+// isLeapYear(2012);
 
 
 
-// 2. Write a JavaScript program to convert temperatures to and from Celsius,
-// Fahrenheit.
-// [ Formula : c/5 = (f-32)/9 [ where c = temperature in Celsius and f = temperature in
-// Fahrenheit ]
+// 2. Write a JavaScript program to convert temperatures to and from Celsius, Fahrenheit.
+// [ Formula : c/5 = (f-32)/9 [ where c = temperature in Celsius and f = temperature in Fahrenheit ]
 // Expected Output :
 // 60°C is 140 °F
 // 45°F is 7.222222222222222°C
+
+function convertTemperature(temp){
+
+    //   C/5 =(F-32)/9 
+    let fahrenheit = (temp*9/5)+32;
+    console.log(`${temp}.C is ${fahrenheit}.F`);
+
+
+    let celcius = (temp-32)*5/9;
+    console.log(`${temp}.F is ${celcius}.C`);
+}
+// convertTemperature(60);
+convertTemperature(45);
 
 
 

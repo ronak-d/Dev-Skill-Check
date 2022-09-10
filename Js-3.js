f516ef7739a90ad3a7d980c80e7c1502f36c3bad
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-09-10 19:27:00 +0530

    q3

--- Js-3.js
+++ Js-3.js
@@ -29,3 +29,7 @@ for(i=1; i<=4; i++) {
 
 // 3. Write a program to find whether a given number is special number or
 // not
+// If the sum of the factorial of digits of a number (N) is equal to the
+// number itself, the number (N) is called a special number.
+// eg- 145 is a special number
+// Logic- 1! + 4! + 5!= 1+24+120 i.e 14

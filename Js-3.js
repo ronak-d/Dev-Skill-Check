4ce037ba283c69fced8391a60fc27a4175d59440
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-09-10 19:26:03 +0530

    q2

--- Js-3.js
+++ Js-3.js
@@ -18,3 +18,11 @@ for(i=1; i<=4; i++) {
 // notThe Armstrong number is a number that is equal to the sum of cubes of
 // its digits. For example 0, 1, 153, 370, 371 and 407 are the Armstrong
 // numbers. Let's try to understand why 153 is an Armstrong number.
+
+// eg => 153 = (1*1*1)+(5*5*5)+(3*3*3) where:
+// (1*1*1)=1
+// (5*5*5)=125
+// (3*3*3)=27
+// So:
+// 1+125+27=153
+q2  

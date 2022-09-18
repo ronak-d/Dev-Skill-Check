38e4ddeb194d9ce36a13cd3ffdddcca7f71a9c26
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-09-18 15:03:04 +0530

    q3

--- Js-3.js
+++ Js-3.js
@@ -4,14 +4,15 @@
 // 2 3
 // 4 5 6
 // 7 8 9 10
-let count = 1;
-for(i=1; i<=4; i++) {
-    let bag = "";
-    for(j=1; j<=i; j++) {
-        bag += count++ +  " ";
-    }
-    // console.log(bag);
-}
+
+// let count = 1;
+// for(i=1; i<=4; i++) {
+//     let bag = "";
+//     for(j=1; j<=i; j++) {
+//         bag += count++ +  " ";
+//     }
+//     // console.log(bag);
+// }
 
 
 // 2. Write a program to find whether a given number is armstrong number or
@@ -26,48 +27,43 @@ for(i=1; i<=4; i++) {
 // So:
 // 1+125+27=153
 
-function findArm(e){
-    let num = e.toString().split('').map(Number);
-    // console.log(typeof num[0]);
-    let sum = 0;
-    let pow = 0;
+// function findArm(e){
+//     let num = e.toString().split('').map(Number);
+//     // console.log(typeof num[0]);
+//     let sum = 0;
+//     let pow = 0;
 
-    for(i=0; i<num.length; i++){
-        pow = num[i]*num[i]*num[i];
-        sum += pow;
-    }
-    return e == sum ? `${e} is armstrong` : `${e} is not armstrong`;
+//     for(i=0; i<num.length; i++){
+//         pow = num[i]*num[i]*num[i];
+//         sum += pow;
+//     }
+//     return e == sum ? `${e} is armstrong` : `${e} is not armstrong`;
 
-    // let sum = num.reduce((ac,cv)=>{
-    //     ac + Math.pow(cv);
-    // },0);
-}
-const armstrong = findArm(370);
+//     // let sum = num.reduce((ac,cv)=>{
+//     //     ac + Math.pow(cv);
+//     // },0);
+// }
+// const armstrong = findArm(370);
 // console.log(armstrong);
 
-// 3. Write a program to find whether a given number is special number or
-// not
-// If the sum of the factorial of digits of a number (N) is equal to the
-// number itself, the number (N) is called a special number.
-// eg- 145 is a special number
+// 3. Write a program to find whether a given number is special number or not.
+// If the sum of the factorial of digits of a number (N) is equal to the number itself, the number (N) is called a special number. eg- 145 is a special number
 // Logic- 1! + 4! + 5!= 1+24+120 i.e 145
 
 let special = (n) => {
 
     let num = n.toString().split("").map(Number);
-    console.log(num);
 
     let value = [];
     let sum = 0;
+    for(let i = 0; i <num.length; i++) {
 
-    let factorial = num.map((e)=>{
+    }
+    for(i=e; i>0; i++){
+        sum += e*(i-1);
+    }
 
-        while(e > 0){
-            sum += (e * e-1);
-        }
-        value.push(sum);
-    })
-    console.log(value);
+    value.push(sum);
 }
 
 let SpecialNum = special(145);

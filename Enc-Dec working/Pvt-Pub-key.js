9d4e1f3f3a00d32d32c57ff896fb8bc07bc4ec02
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-09-30 02:30:27 +0530

    encryption-decryption done.

--- Enc-Dec working/Pvt-Pub-key.js
+++ Enc-Dec working/Pvt-Pub-key.js
@@ -4,17 +4,33 @@ const encryptRsa = new EncryptRsa();
 
 // console.log(encryptRsa);
 
-const { privateKey, publicKey:publicKey1 } = encryptRsa.createPrivateAndPublicKeys();
+const { privateKey:privateKey1, publicKey:publicKey1 } = encryptRsa.createPrivateAndPublicKeys();
 
-let message = 'E2E Encryption';
-const encryptedText = encryptRsa.encryptStringWithRsaPublicKey({ 
+let message = 'E2E Encryption with both keys';
+const encryptedText1 = encryptRsa.encryptStringWithRsaPublicKey({ 
   text: message,   
   publicKey:publicKey1
 });
 // console.log(encryptedText);
 
-const decryptedText = encryptRsa.decryptStringWithRsaPrivateKey({ 
-  text: encryptedText, 
-  privateKey
+const decryptedText1 = encryptRsa.decryptStringWithRsaPrivateKey({ 
+  text: encryptedText1, 
+  privateKey:privateKey1
 });
-console.log(decryptedText);
+console.log(decryptedText1);
+
+// ----------------------
+const { privateKey:privateKey2, publicKey:publicKey2 } = encryptRsa.createPrivateAndPublicKeys();
+
+let message2 = 'How to implement yrr';
+const encryptedText2 = encryptRsa.encryptStringWithRsaPublicKey({ 
+  text: message2,   
+  publicKey:publicKey2
+});
+// console.log(encryptedText);
+
+const decryptedText2 = encryptRsa.decryptStringWithRsaPrivateKey({ 
+  text: encryptedText2, 
+  privateKey:privateKey2
+});
+console.log(decryptedText2);

e6713746e9071f7531e51733456898c2ac183227
ronak-d <ronakdodeja73@gamil.com>
2022-10-20 18:05:46 +0530

    made a contract to receive

--- Local Setup Using Hardhat/scripts/deploy.js
+++ Local Setup Using Hardhat/scripts/deploy.js
@@ -15,17 +15,28 @@ async function main() {
 
   const lockedAmount = hre.ethers.utils.parseEther("1");
 
+  // Contract factory is created in {contracts}.json file by hardhat.
   const CRUD = await hre.ethers.getContractFactory("CRUD");
   // inside deploy constructor args has to passed with deploy() funcn;
   const crud = await CRUD.deploy();
   // now this crud function is fully deployed.
   await crud.deployed();
 
-  console.log(crud);
+  // console.log(crud);
 
   console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${crud.address}`
+    `Crud contract timestamp ${unlockTime} deployed to ${crud.address}`
   );
+
+  const createEmployeeGiveArgs = await crud.create("ron","0x24794203d1246ee2625860D74415356b5996AFf6")
+
+  const countTotalEmployees = await crud.totalepmployees;
+
+  const ShowEmployee = await crud.employees(0);
+
+  console.log(countTotalEmployees);
+
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere

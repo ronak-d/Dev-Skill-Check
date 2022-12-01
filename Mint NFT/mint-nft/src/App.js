fefb1153339bc467d2344883b38a6f2311305694
ronak-d <ronakdodeja73@gamil.com>
2022-12-01 23:31:42 +0530

    connected from rainbow

--- Mint NFT/mint-nft/src/App.js
+++ Mint NFT/mint-nft/src/App.js
@@ -1,11 +1,47 @@
-import './App.css';
+import React from 'react'
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import {
+  chain,
+  configureChains,
+  createClient,
+  WagmiConfig,
+} from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+import ConnectBtn from './Components/ConnectBtn';
 
 function App() {
+
+  const { chains, provider } = configureChains(
+    [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum, chain.goerli],
+    [
+      alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
+      publicProvider()
+    ]
+  );
+  const { connectors } = getDefaultWallets({
+    appName: 'mint-nft',
+    chains
+  });
+  const wagmiClient = createClient({
+    autoConnect: true,
+    connectors,
+    provider
+  })
+
   return (
     <div className="App">
-      hello react
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <ConnectBtn />
+      </RainbowKitProvider>
+    </WagmiConfig>
     </div>
   );
 }
 
-export default App;
+export default App

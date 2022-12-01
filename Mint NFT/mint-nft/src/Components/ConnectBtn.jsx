a4b172affa6010890fc2d5dc64f56d08f9f3165c
ronak-d <ronakdodeja73@gamil.com>
2022-12-01 23:31:28 +0530

    Connect btn solved

---
+++ Mint NFT/mint-nft/src/Components/ConnectBtn.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
+const ConnectBtn = () => {
+  return (
+    <div>{ConnectButton}</div>
+  )
+}
+
+export default ConnectBtn;
